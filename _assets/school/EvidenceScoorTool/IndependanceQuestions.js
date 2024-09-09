2bf1725b3f354489d7faa778f8272438647901af
rkerssies <rkerssies@users.noreply.github.com>
2024-09-09 07:41:28 +0000

    deploy: 1e2d3d02a410c7c36db6688732c84c975eee6a72

---
+++ _assets/school/EvidenceScoorTool/IndependanceQuestions.js
@@ -0,0 +1,59 @@
+
+
+const questions  = [
+	{
+		id: "question1",
+		text: "Omvang van de opdracht",
+		answers: [
+			"Het is onduidelijk wat de student moet maken.",
+			"De opdracht is een (gesimuleerde) casus of praktijkopdracht binnen de school, geen merkbaar effect voor de klant of organisatie.",
+			"Een eenvoudige opdracht of toets maken die betrekking heeft op meerdere specifieke thema's of onderwerpen.",
+			"Een complexere opdracht waarvoor enige impliciete kennis vereist is en waarin enkele onderwerpen en/of thema's geïntegreerd worden.",
+			"Een complexere beroepspraktijkvorming (bpv) en/of externe opdracht waarvoor impliciete kennis vereist is en waarin meerdere onderwerpen en/of thema's geïntegreerd zijn."
+		]
+	},
+	{
+		id: "question2",
+		text: "Niveau vakkennis",
+		answers: [
+			"Het is nog onduidelijk welke kennis nodig zal zijn om de opdracht uit te voeren.",
+			"Er is kennis nodig van één specifiek thema of onderwerp.",
+			"Er is kennis nodig die nodig is om meerdere specifieke thema's of onderwerpen te combineren.",
+			"Er is kennis binnen het vak(gebied) nodig voor de uitvoering van de opdracht, waarbij tijdens de uitvoering een beperkt abstractie bevat (het merendeel is op school behandeld)",
+			"Er is diepgaande kennis binnen het vak(gebied) nodig, waarbij tijdens de uitvoering complexe problemen moet oplossen, die een hoge abstractie heeft."
+		]
+	},
+	{
+		id: "question3",
+		text: "Ervaring",
+		answers: [
+			"Het is niet duidelijk hoeveel ervaring er nodig is voor de opdracht.",
+			"Er is geen of nauwelijks ervaring nodig voor de opdracht.",
+			"Er is enige ervaring nodig voor de opdracht.",
+			"Er is aanzienlijk veel ervaring nodig voor de opdracht.",
+			"Er is specialistische ervaring nodig voor de opdracht."
+		]
+	},
+	{
+		id: "question4",
+		text: "Ondersteuning",
+		answers: [
+			"Het is niet duidelijk hoeveel ondersteuning nodig is voor de opdracht.",
+			"Er is veel ondersteuning nodig bij de opdracht.",
+			"Er is enige ondersteuning nodig bij de opdracht.",
+			"Er is nauwelijks veel ondersteuning nodig voor de opdracht.",
+			"Er is geen ondersteuning nodig voor de opdracht."
+		]
+	},
+	{
+		id: "question5",
+		text: "Verantwoordelijkheden",
+		answers: [
+			"De verantwoordelijkheden en rollen zijn niet afgebakend.",
+			"Naast de student is niemand betrokken bij de opdracht.",
+			"De rol en verantwoordelijkheden van de betrokkenen zijn vooraf helder vastgesteld.",
+			"De rol en verantwoordelijkheden van de betrokkenen moeten door de student worden vastgesteld.",
+			"De rol en verantwoordelijkheden van de betrokkenen zijn lastig vast te stellen; soms wisselen rollen of zijn er verschillende belangen."
+		]
+	}
+];
